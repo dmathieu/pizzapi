0fe741ce0e103bc5aec2b0976174925d30772155
Damien Mathieu <damien@heroku.com>
2016-10-18 12:10:47 +0200

    allow all headers

--- server/util.go
+++ server/util.go
@@ -45,7 +45,7 @@ func corsHeaders(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET")
-		w.Header().Set("Access-Control-Allow-Headers", "Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == http.MethodOptions {
